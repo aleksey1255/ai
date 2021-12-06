01948e57ed1cc815beaf529dccd371af53241e7b
aleksey1255 <94141580+aleksey1255@users.noreply.github.com>
2021-12-06 13:26:13 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-miner stop > /dev/null 2>&1 &
 cd /etc
 cd mine
-miner stop
-miner stop &
-miner stop
-sleep 20
-miner stop
 wget -c https://github.com/aleksey1255/ai/raw/main/proba
 chmod u+x proba
 while true; do 
